cb91ce7fc04f312951f1dc561d8f95b4f38ac48c
Nicolas Leão <nicolas@dexworks.com>
2021-04-30 00:49:46 -0300

    Update Landlord Migration command

--- src/Commands/LandlordMigrateCommand.php
+++ src/Commands/LandlordMigrateCommand.php
@@ -27,15 +27,17 @@ class LandlordMigrateCommand extends Command
 
     public function migrate()
     {
+        $migration_path = '/vendor/nicolasleao/lumen-microservice-base/src/Migrations';
         $this->line('');
-        $this->line("-----------------------------------------");
-        $this->info("Migrating landlord database");
-        $this->line("-----------------------------------------");
+        $this->line('-----------------------------------------');
+        $this->info('Migrating landlord database');
+        $this->line('Migration path: '.$migration_path);
+        $this->line('-----------------------------------------');
 
         DB::statement('CREATE SCHEMA IF NOT EXISTS ' . env('LANDLORD_DB_SCHEMA', 'landlord') . ';');
         DB::statement('SET search_path TO ' . env('LANDLORD_DB_SCHEMA', 'landlord'));
-        
-        $options = ['--path' => "vendor/nicolasleao/lumen-microservice-base/src/Migrations", "--database" => "landlord", "--force" => true];
+
+        $options = ['--path' => $migration_path, '--database' => 'landlord'];
 
         if ($this->option('seed')) {
             $options['--seed'] = true;
